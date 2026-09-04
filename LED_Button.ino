55fab0ec2d14c185f3c832a8919da3071f6dfee9
Sahilsaliembedded <sahilvsali@gmail.com>
2026-09-04 19:44:12 +0530

    add Button controlled Led Program

---
+++ LED_Button.ino
@@ -0,0 +1,16 @@
+#define LED_PIN 2
+#define BUTTON_PIN 4
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+}
+
+void loop() {
+  if (digitalRead(BUTTON_PIN) == LOW) {
+    digitalWrite(LED_PIN, HIGH);
+  } 
+  else {
+    digitalWrite(LED_PIN, LOW);
+  }
+}
